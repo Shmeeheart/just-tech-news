fe26bdcb66575d275813ba2fb1ad36ccc0c1578a
Shmeeheart <Shmeeheart.goodwije@gmail.com>
2022-07-23 13:33:59 -0600

    feature/comments

--- models/post.js
+++ models/post.js
@@ -1,16 +1,15 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-
 // create our Post model
 class Post extends Model {
   static upvote(body, models) {
     return models.Vote.create({
       user_id: body.user_id,
-      post_id: body - post_id,
+      post_id: body.post_id,
     }).then(() => {
       return Post.findOne({
         where: {
-          id: body - post_id,
+          id: body.post_id,
         },
         attributes: [
           'id',
@@ -23,6 +22,22 @@ class Post extends Model {
             ),
             'vote_count',
           ],
+        ],
+        include: [
+          {
+            model: models.Comment,
+            attributes: [
+              'id',
+              'comment_text',
+              'post_id',
+              'user_id',
+              'created_at',
+            ],
+            include: {
+              model: models.User,
+              attributes: ['username'],
+            },
+          },
         ],
       });
     });
